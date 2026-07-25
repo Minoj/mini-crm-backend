c8215961947715cd80b04a8d0510a5f9d3a53894
humoyun <rakhmatullayev1996@gmail.com>
2026-07-25 19:12:08 +0500

    Fix var/ folder permissions in start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,15 @@ cd /var/www/html
 
 touch .env
 
+mkdir -p var/cache var/log
+chmod -R 777 var/
+
 if [ ! -f config/jwt/private.pem ]; then
     mkdir -p config/jwt
     php bin/console lexik:jwt:generate-keypair --no-interaction
 fi
+
 php bin/console doctrine:migrations:migrate --no-interaction
+
 php-fpm -D
 nginx -g "daemon off;"
